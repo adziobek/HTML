e663da7ea090b1466874c7661aecf7428df6a168
adziobek <adziobek@apreel.com>
2017-07-30 20:35:29 +0200

    DOM: script async and defer attributes

--- DOM/external.js
+++ DOM/external.js
@@ -1,2 +1,11 @@
 console.log("Parser download external .js and run it");
 document.write("<p>This is from external .js</p>");
+
+window.onload = function () {
+    changeptext("window.onload after DOM was created");
+};
+changeptext("while DOM is created and html is parsed");
+function changeptext(newtext) {
+    var ptext = document.getElementById("ptext");
+    ptext.innerHTML = newtext;
+}
