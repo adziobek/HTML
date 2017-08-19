69ca71df9eccee5c242f05d19ffa7b21329e7955
adziobek <adziobek@apreel.com>
2017-08-19 20:08:02 +0200

    DOM: Event interface - Event.currentTarget test and bubbling test

--- DOM/events.js
+++ DOM/events.js
@@ -14,6 +14,22 @@ window.onload = function () {
     customDiv.addEventListener('customClickEvent', function (event) {
         console.log("Event type=[" + event.type + "] handling by custom pink div");
     }, false);
+
+    var div1 = document.getElementById("div1");
+    var div2 = document.getElementById("div2");
+    var div3 = document.getElementById("div3");
+
+    div1.addEventListener("click", function (event) {
+        console.log("div1 handler");
+    });
+
+    div2.addEventListener("click", function (event) {
+        console.log("div2 handler");
+    }, true);
+
+    div3.addEventListener("click", function (event) {
+        console.log("div3 handler");
+    });
 };
 
 //Obsługiwanie domyślenego zdarzenia 'click'
