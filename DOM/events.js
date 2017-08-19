78d91c0d19dfeec95ae88a790145c25b249665cc
adziobek <adziobek@apreel.com>
2017-08-19 16:16:08 +0200

    DOM: Event interface part 2 -create and dispatch custom Event -handle event by JavaScript function or addEventListener -event.stopPropagation .preventDefault

---
+++ DOM/events.js
@@ -0,0 +1,44 @@
+window.onload = function () {
+    /*Rejestruje w onload customListenera by nie dodawać listenera
+     * za każdym razem gdy wystąpi zdarzenie i zostanie wywołana, np.
+     * metoda onclick()
+     * */
+    var customButton = document.getElementById("customButton");
+    var customDiv = document.getElementById("customDiv");
+
+    customButton.addEventListener('customClickEvent', function (event) {
+        console.log("Event type=[" + event.type + "] handling by custom button");
+        console.log(event);
+    }, false);
+
+    customDiv.addEventListener('customClickEvent', function (event) {
+        console.log("Event type=[" + event.type + "] handling by custom pink div");
+    }, false);
+};
+
+//Obsługiwanie domyślenego zdarzenia 'click'
+function handleButtonClick(event) {
+    console.log("<>< <>< <><");
+    console.log("Event type=[" + event.type + "] handling by button");
+}
+
+function handleYellowDivClick(event) {
+    console.log("Event type=[" + event.type + "] handling by yellow div");
+    console.log(event);
+}
+
+function handlePinkDivClick(event) {
+    console.log("Event type=[" + event.type + "] handling by pink div");
+}
+
+function invokeCustomClickEvent(defaultClickEvent) {
+    //Najpierw obsługujemy domyślne zdarzenia click na buttonie
+    console.log("<>< <>< <><");
+    console.log("Event [" + defaultClickEvent.type + "] handling by custom button");
+    defaultClickEvent.stopPropagation();
+
+    console.log("invokeCustomClickEvent()");
+    var customClickEvent = new Event("customClickEvent", {"bubbles": true, "cancelable": true}); //tworzymy i inicjalizujemy Event
+    var customButton = document.getElementById("customButton");
+    customButton.dispatchEvent(customClickEvent); //wywołujemy Event na elemencie
+}
